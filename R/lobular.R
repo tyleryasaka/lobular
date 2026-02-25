73b591e0f73c477ad5290603e9af42e9f4b745bb
Tyler Yasaka <contact@tyleryasaka.me>
2026-02-24 22:01:46 -0500

    Update lobular.R

--- R/lobular.R
+++ R/lobular.R
@@ -197,7 +197,7 @@ apply_interpolation = function(mtx, coords, zone_obj, resolution = 1) {
 #' @param factor_threshold (Optional) Minimum value for zonation factors to be included in calculation (removes noise).
 #' @return A \code{ZonationObject} with calibrated baseline zonation
 #' @export
-setBaseline = function(mtx, norm_mtx = NULL, coords = NULL, species = 'human', regularization = 0.8, verbose = FALSE) {
+setBaseline = function(mtx, norm_mtx = NULL, coords = NULL, species = 'human', regularization = 0.85, verbose = FALSE) {
   if (species == 'human') {
     initial_weights = readRDS(system.file('extdata', 'initial_weights_human.RDS', package = 'lobular'))
   } else if (species == 'mouse') {
